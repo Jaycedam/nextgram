ecda59d044f8a122b5ebdbd000078665e3428fa1
Jay <45575946+Jaycedam@users.noreply.github.com>
2024-02-01 21:37:12 -0300

    chore: generateStaticParams for photos

--- app/@modal/(.)photos/[id]/page.tsx
+++ app/@modal/(.)photos/[id]/page.tsx
@@ -1,9 +1,14 @@
-import { Modal } from './modal';
+import { Modal } from "./modal";
 
-export default function PhotoModal({
-  params: { id: photoId },
-}: {
-  params: { id: string };
-}) {
-  return <Modal>{photoId}</Modal>;
+export default function PhotoModal({ params }: { params: { id: string } }) {
+  const { id } = params;
+  return <Modal>{id}</Modal>;
+}
+
+export async function generateStaticParams() {
+  const photos = Array.from({ length: 6 }, (_, i) => ({ id: String(i + 1) }));
+
+  return photos.map((photo) => ({
+    id: photo.id,
+  }));
 }
--- app/photos/[id]/page.tsx
+++ app/photos/[id]/page.tsx
@@ -1,7 +1,12 @@
-export default function PhotoPage({
-  params: { id },
-}: {
-  params: { id: string };
-}) {
+export default function PhotoPage({ params }: { params: { id: string } }) {
+  const { id } = params;
   return <div className="card">{id}</div>;
+}
+
+export async function generateStaticParams() {
+  const photos = Array.from({ length: 6 }, (_, i) => ({ id: String(i + 1) }));
+
+  return photos.map((photo) => ({
+    id: photo.id,
+  }));
 }
